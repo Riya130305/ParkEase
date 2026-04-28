c93788e1f3196ecb1a8581b9028c3ce577e31c48
Rupendra Yadav <rupdendrayadav1022@gmail.com>
2026-04-28 13:06:46 +0530

    created repository

---
+++ backend/src/main/java/com/parking/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.parking.repository;
+
+import com.parking.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
